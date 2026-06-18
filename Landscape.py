ccb1ab2457056bcfbda6caae88aac395ee17e57f
downputt <downputtgd@gmail.com>
2026-06-18 18:33:58 +1000

    Add files via upload

--- Landscape.py
+++ Landscape.py
@@ -1,17 +1,23 @@
 import pgzrun
+import pygame
 
 WIDTH = 300
 HEIGHT = 300
 
-def mountain():
+def draw():
+    screen.fill((135, 206, 250))
+
     point1 = (50,150)
-    point2 = (150,150)
+    point2 = (150,50)
     point3 = (75,150)
-    screen.draw.filled_polygon([point1, point2, point3], (121, 114, 113))
-    point21 = (,150)
-    point22 = (150,150)
-    point23 = (75,150)
-    screen.draw.filled_polygon([point21, point22, point23], (121, 114, 113))
+    pygame.draw.polygon([point1, point2, point3], (121, 114, 113))
+    point21 = (250,150)
+    point22 = (150,50)
+    point23 = (225,150)
+    pygame.draw.polygon([point21, point22, point23], (121, 114, 113))
 
+    screen.draw.filled_circle((150,75),25,(255,200,100))
 
-pgzrun.go
+    screen.draw.filled_rect(Rect((0,150), (300, 300)), (50, 150, 50))
+
+pgzrun.go()
