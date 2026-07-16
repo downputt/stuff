7e978e84d25c4da9a80716211ed4414984250284
downputt <downputtgd@gmail.com>
2026-07-16 18:57:41 +1000

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,13 @@
+quiz = [
+    ("What piece of technology was repopularised in 2026?",
+     ("A. Personal Computers","B. Wired Earphones","C. Blu-ray Movie Players", "D. CRT TVs"), 
+     "B"),
+    
+    ("What kind of food is NOT widespread?",
+     ("A. Pasta", "B. Ice Cream", "C. Burger", "D. Grasshopper"), 
+     "D"),
+
+    ("What is the meaning of the acronym 'ESA?'",
+     ("A. European Space Agency", "B. Emergency Services Authority", "C. Emergency Shelter Administration", "D. Economic Crisis Alliance"),
+     "A")
+]
