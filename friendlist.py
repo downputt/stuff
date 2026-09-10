79d3fece3e2b012cf0ebf7cbf7b7103a24e480ce
downputt <downputtgd@gmail.com>
2026-09-10 18:04:37 +1000

    Add files via upload

---
+++ friendlist.py
@@ -0,0 +1,29 @@
+import time
+import os
+
+def load():
+    for i in range(2):
+        print(".")
+        time.sleep(0.2)
+        os.system("clear")
+        print(".")
+        time.sleep(0.2)
+        os.system("clear")
+        print(".")
+        time.sleep(0.2)
+        os.system("clear")
+        print(".")
+        time.sleep(0.2)
+        os.system("clear")
+        print(".")
+        time.sleep(0.2)
+        os.system("clear")
+        print(".")
+        time.sleep(0.2)
+        os.system("clear")
+        print(".")
+        time.sleep(0.2)
+        os.system("clear")
+        print(".")
+        time.sleep(0.2)
+        os.system("clear")
